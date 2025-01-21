f31638e27503590ee0baf3548a973072d3e5cdc8
Helder Balbino <helderbalbino@googlemail.com>
2025-01-21 15:12:14 +0000

    add: implement addContact function to retrieve client input values

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ const form = document.getElementById("client-form");
 
 const contactList = document.getElementById("contact-list");
 
-function addContact(event) {}
+function addContact(event) {
+    const name = document.getElementById("name").value;
+    const phone = document.getElementById("phone").value;
+    const email = document.getElementById("email").value;
+}
